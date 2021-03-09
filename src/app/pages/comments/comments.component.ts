dab5966e4eff30506918cba43c847b66eb1e8c83
HelmiSassi <c.helmi@live.fr>
2021-03-09 09:55:46 +0100

    init project

---
+++ src/app/pages/comments/comments.component.ts
@@ -0,0 +1,52 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {CommentsModel} from 'src/app/models/comments.model';
+import {NewsModel} from 'src/app/models/news.model';
+import {CommentsService} from 'src/app/services/comments.service';
+import {NewsService} from 'src/app/services/news.service';
+import {UserModel} from 'src/app/models/user.model';
+import {UserService} from 'src/app/services/user.service';
+import { RoleEnum } from 'src/app/enum/role.enum';
+
+@Component({
+  selector: 'app-comments',
+  templateUrl: './comments.component.html',
+  styleUrls: ['./comments.component.css']
+})
+export class CommentsComponent implements OnInit {
+  @Input()
+  news: NewsModel = {};
+  comments: CommentsModel = {};
+  answer: CommentsModel = {};
+  user: UserModel;
+  roleEnum = RoleEnum;
+
+  constructor(private commentsService: CommentsService,
+              private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.user = this.userService.getConnectedUser();
+  }
+
+  add() {
+    this.commentsService.add(this.news._id, this.comments).subscribe(data => {
+      if (!this.news.comments) {
+        this.news.comments = [];
+      }
+      this.news.comments.unshift(data);
+      this.comments = {};
+    });
+  }
+
+  // addAnswer(){
+  //   this.commentsService.add(this.comments.articleId, this.answer).subscribe(data =>{
+  //     if(!this.comments.answer){
+  //       this.comments.answer = [];
+  //     }
+  //      this.comments.answer.push(data);
+  //      this.answer = {}
+  //   })
+  // }
+}
+
